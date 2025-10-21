b28716609e13c46bf2e5a517f5c058801aff8031
mathai50 <doc.mathew@hotmail.com>
2025-10-21 13:16:23 +0530

    Refactor: Remove backend services, Nix/Bmad system, and multi-service files

--- services/index.ts
+++
@@ -1,4 +0,0 @@
-// TODO: Restore these service exports if needed for other features
-// export * from './geminiService';
-// export * from './serpApiService';
-// export * from './pagespeedApiService';
